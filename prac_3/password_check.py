22c9231f61d600d3f3aca4f63523178ac528c265
dpitcrowd <listenupimbusy@gmail.com>
2020-04-11 10:27:03 +1000

    Add password check program

--- prac_3/password_check.py
+++ prac_3/password_check.py
@@ -5,15 +5,9 @@ Password checker built from "skeleton" code to help you get started
 
 """ My constants"""
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
-MIN_LENGTH = 1
-MAX_LENGTH = 1
-UPPER_CASE = "False"
-LOWER_CASE = "False"
-SPECIAL_CHARS_REQUIRED = "False"
-DIGIT = 0
 
 
-def is_valid_password(password):
+def get_password(password, MIN_LENGTH, MAX_LENGTH, UPPER_CASE, LOWER_CASE, SPECIAL_CHARS_REQUIRED, DIGIT):
     """Determine if the provided password is valid."""
 
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
@@ -30,6 +24,8 @@ def is_valid_password(password):
             digit += 1
         elif i.isupper():
             password_upper += 1
+        elif i.islower():
+            password_lower += 1
         elif i in SPECIAL_CHARACTERS:
             special_characters += 1
 
@@ -51,8 +47,16 @@ def is_valid_password(password):
 
 
 def main():
-
     """Program to get and check a user's password."""
+
+    """ My local variables"""
+    MIN_LENGTH = 1
+    MAX_LENGTH = 1
+    UPPER_CASE = "False"
+    LOWER_CASE = "False"
+    SPECIAL_CHARS_REQUIRED = "False"
+    DIGIT = 0
+
     print("This script allows you to analyze your password and see if it meets the required parameters. ")
     MIN_LENGTH = int(input("Enter min length for your password --> "))
     MAX_LENGTH = int(input("Enter max length for your password --> "))
@@ -61,6 +65,7 @@ def main():
         UPPER_CASE = input("Do you want to use at least one upper case character (y/n) n --> ")
         if UPPER_CASE == "Y" or UPPER_CASE == "y":
             UPPER_CASE = "True"
+            MAX_LENGTH += 1
         else:
             UPPER_CASE = "False"
         break
@@ -69,6 +74,7 @@ def main():
         LOWER_CASE = input("Do you want to use at least one lower case character (y/n) n --> ")
         if LOWER_CASE == "Y" or LOWER_CASE == "y":
             LOWER_CASE = "True"
+            MAX_LENGTH += 1
         else:
             LOWER_CASE = "False"
         break
@@ -77,6 +83,7 @@ def main():
         SPECIAL_CHARS_REQUIRED = input("Do you want to use a special character (y/n) n --> ")
         if SPECIAL_CHARS_REQUIRED == "Y" or SPECIAL_CHARS_REQUIRED == "y":
             SPECIAL_CHARS_REQUIRED = "True"
+            MAX_LENGTH += 1
         else:
             SPECIAL_CHARS_REQUIRED = "False"
         break
@@ -84,13 +91,14 @@ def main():
         DIGIT = input("Do you want to use at least one digit (y/n) n --> ")
         if DIGIT == "Y" or DIGIT == "y":
             DIGIT = "True"
+            MAX_LENGTH += 1
         else:
             DIGIT = "False"
         break
 
     password = input("Enter your password --> ")
 
-    while not is_valid_password(password):
+    while not get_password(password, MIN_LENGTH, MAX_LENGTH, UPPER_CASE, LOWER_CASE, SPECIAL_CHARS_REQUIRED, DIGIT):
         print("Invalid password!")
         password = input("Enter your password --> ")
     print("Your password is valid and made of {} characters and is {}".format(len(password), password))
