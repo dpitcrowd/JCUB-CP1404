156f4d8895586981d48bb7c5fa40ba6a57e01f33
dpitcrowd <listenupimbusy@gmail.com>
2020-04-18 17:49:21 +1000

    subject_reader.py v1.00

---
+++ prac_4/subject_reader.py
@@ -0,0 +1,44 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    #print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    print(data)
+    my_data = my_function(data)
+
+
+def get_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    input_file = open(FILENAME)
+    my_list = []
+
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------\n")
+        my_list.append(parts)
+
+    return my_list
+
+    input_file.close()
+
+
+def my_function(data):
+
+    for x in data:
+        print("{} is taught by {:>12} and has {:>3} students".format(x[0], x[1], x[2]))
+
+
+
+main()
